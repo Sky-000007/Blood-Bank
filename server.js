6188cfd35c7d6f9e156bc8eb17feb52fe1c64e12
Sky-000007 <shailendrastm07@gmail.com>
2023-10-20 18:45:07 +0530

    Project Completed

--- server.js
+++ server.js
@@ -27,14 +27,6 @@ app.use('/api/v1/inventory',require('./routes/inventoryRoutes'))
 app.use("/api/v1/analytics", require("./routes/analyticsRoutes"));
 app.use("/api/v1/admin", require("./routes/adminRoutes"));
 
-//static folder
-app.use(express.static(path.join(__dirname,"./frontend/build")))
-
-//static routes
-app.get("*",function(req,res){
-  res.sendFile(path.join(__dirname,'./frontend/build/index.html'))
-})
-
 //port
 const PORT = process.env.PORT || 8080;
 const DEV_MODE = process.env.DEV_MODE
